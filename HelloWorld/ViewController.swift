a57a07f002f6886c59fc7f617258a5412f44770e
Nikolas Ponomarov <mponomar@Dobraas-MacBook-Pro.local>
2017-08-17 00:37:01 +0300

    first basic project

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nikolas Ponomarov on 14.08.17.
+//  Copyright © 2017 Nikolas Ponomarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var titleimage: UIImageView!
+    @IBOutlet weak var welcomeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func welcomePressed(_ sender: Any) {
+        titleimage.isHidden = false
+        welcomeButton.isHidden = true
+        background.isHidden = false
+    }
+    
+}
+
