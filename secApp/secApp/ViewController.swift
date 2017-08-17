61088b039c1033adbb586cc6558d6077a72ba36b
Nikolas Ponomarov <mponomar@Dobraas-MacBook-Pro.local>
2017-08-18 00:10:19 +0300

    add secProject

---
+++ secApp/secApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  secApp
+//
+//  Created by Nikolas Ponomarov on 17.08.17.
+//  Copyright © 2017 Nikolas Ponomarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextFiled: UITextField!
+    @IBOutlet weak var nameLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
